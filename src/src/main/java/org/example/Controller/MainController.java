3f227b097a248cff64e82d7d412298a1f9bf3c47
felix <felix@something.com>
2026-01-09 20:35:05 +0100

    move files and add UML diagrams, create simple structure to improve later

---
+++ src/src/main/java/org/example/Controller/MainController.java
@@ -0,0 +1,13 @@
+package org.example.Controller;
+
+/**
+ * Instianciate the app the first time
+ */
+public class MainController {
+
+    public MainController() {
+        System.out.println("SUCCESS !");
+    }
+
+
+}
---
+++ src/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.example.Controller.MainController;
+
+/**
+ * @version Java 21 (Microsoft OpenJdk 21.0.9)
+ */
+public class Main {
+    public static void main(String[] arg) {
+        new MainController();
+    }
+}
