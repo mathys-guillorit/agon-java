f3797d93ae2e22523974f3eb9e25ca93d3cf2ce2
Lukas Soetens <soetenslukas@gmail.com>
2026-01-30 18:13:11 +0100

    architecture projet

--- agon/src/main/java/univ/bordeaux/fr/Example.java
+++ agon/src/main/java/univ/bordeaux/fr/Example.java
@@ -2,39 +2,39 @@ package univ.bordeaux.fr;
 
 public class Example {
 
-    private int value;
-    private int rd;
+  private int value;
+  private int rd;
 
-    public Example(){
-        value = 0;
-        rd = this.randint(1,100);
-    }
+  public Example() {
+    value = 0;
+    rd = this.randint(1, 100);
+  }
 
-    public void compute(){
-        this.value = this.rd/this.value;
-    }
+  public void compute() {
+    this.value = this.rd / this.value;
+  }
 
-    private int randint(int min, int max){
-        return (int) (Math.random() * (max - min + 1)) + min;
-    }
+  private int randint(int min, int max) {
+    return (int) (Math.random() * (max - min + 1)) + min;
+  }
 
-    public int add(int a, int b) {
-        return a + b;
-    }
+  public int add(int a, int b) {
+    return a + b;
+  }
 
-    public int getValue() {
-        return value;
-    }
+  public int getValue() {
+    return value;
+  }
 
-    public void setValue(int value) {
-        this.value = value;
-    }
+  public void setValue(int value) {
+    this.value = value;
+  }
 
-    public int getRd() {
-        return rd;
-    }
+  public int getRd() {
+    return rd;
+  }
 
-    public void setRd(int rd) {
-        this.rd = rd;
-    }
+  public void setRd(int rd) {
+    this.rd = rd;
+  }
 }
