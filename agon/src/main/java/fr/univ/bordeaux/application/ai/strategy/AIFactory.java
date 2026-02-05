81a95e6a3ad099dd17acc5a18565286b7a4bad9f
Milan Théodore Delage <milan.theodore-delage@etu.u-bordeaux.fr>
2026-02-05 10:23:17 +0100

    comments

--- agon/src/main/java/fr/univ/bordeaux/application/ai/strategy/AIFactory.java
+++ agon/src/main/java/fr/univ/bordeaux/application/ai/strategy/AIFactory.java
@@ -6,10 +6,12 @@ import fr.univ.bordeaux.application.ai.interfaces.BoardEvaluator;
 public class AIFactory {
 
     public AgonAI createAI(AiDecoder config){
+        //Crée le bon model d'ia en fonction des parametres passés dans la config
         return null;
     }
 
     private BoardEvaluator createEvaluator(String type){
+        //Appelé par createAI pour créer la fonction d'évaluation utile au model
         return null;
     }
 }
