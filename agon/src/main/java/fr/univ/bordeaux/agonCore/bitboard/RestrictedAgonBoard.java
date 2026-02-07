6ee4c634c49f66662648e0b88ec587b70412f425
Lukas Soetens <soetenslukas@gmail.com>
2026-02-07 14:35:28 +0100

    fix pipelines

--- agon/src/main/java/fr/univ/bordeaux/agonCore/bitboard/RestrictedAgonBoard.java
+++ agon/src/main/java/fr/univ/bordeaux/agonCore/bitboard/RestrictedAgonBoard.java
@@ -1,6 +1,7 @@
 package fr.univ.bordeaux.agonCore.bitboard;
 
 import fr.univ.bordeaux.agonCore.agonElements.Color;
+import fr.univ.bordeaux.agonCore.agonElements.Move;
 import fr.univ.bordeaux.agonCore.agonElements.PieceType;
 import java.util.List;
 public interface RestrictedAgonBoard{
