9d578f219c504ded75ca073e7daf3c082fc94a92
Felix Ton That-Lavarini <felix.ton-that-lavarini@etu.u-bordeaux.fr>
2026-03-12 12:11:11 +0100

    sync project with main (moved project into main)

---
+++ src/main/java/fr/univ/bordeaux/agonCore/agonElements/Color.java
@@ -0,0 +1,17 @@
+package fr.univ.bordeaux.agonCore.agonElements;
+
+/**
+ * Defines the two competing sides in the Agon game.
+ * * <p>This enum is used to identify piece ownership, track the current player's turn,
+ * and evaluate game states from specific perspectives.</p>
+ */
+public enum Color {
+  /** * Represents the Black player.
+   */
+  BLACK,
+
+  /** * Represents the White player.
+   */
+  WHITE;
+
+}
---
+++ src/main/java/fr/univ/bordeaux/ui/AbstractGameUI.java
@@ -0,0 +1,76 @@
+package fr.univ.bordeaux.ui;
+
+public abstract class AbstractGameUI implements GameUserInterface {
+
+  private GameUserInterface gameEngine;
+
+  public void setGameEngine(GameUserInterface gameEngine) {
+    this.gameEngine = gameEngine;
+  }
+
+  protected GameUserInterface getGameEngine() {
+    if (this.gameEngine == null) {
+      throw new IllegalStateException("The game engine is disconnected");
+    }
+    return this.gameEngine;
+  }
+
+  public abstract void start();
+
+  @Override
+  public void startNewGame(String[] args) {
+    getGameEngine().startNewGame(args);
+  }
+
+  /*
+
+      @Override public void tryMove(Position from, Position to) {
+          getGameEngine().tryMove(from, to);
+      }
+
+      @Override public void selectPiece(Position pos) {
+          getGameEngine().selectPiece(pos);
+      }
+
+  */
+
+  @Override
+  public void undo() {
+    getGameEngine().undo();
+  }
+
+  @Override
+  public void redo() {
+    getGameEngine().redo();
+  }
+
+  @Override
+  public void saveGame(String f) {
+    getGameEngine().saveGame(f);
+  }
+
+  @Override
+  public void loadGame(String f) {
+    getGameEngine().loadGame(f);
+  }
+
+  @Override
+  public void pauseGame() {
+    getGameEngine().pauseGame();
+  }
+
+  @Override
+  public void resumeGame() {
+    getGameEngine().resumeGame();
+  }
+
+  @Override
+  public void quitGame() {
+    getGameEngine().quitGame();
+  }
+
+  @Override
+  public void requestHint() {
+    getGameEngine().requestHint();
+  }
+}
